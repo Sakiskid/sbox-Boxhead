4800e0a40b0d1c631978972db9588f0a94c2c5c7
Sakiskid <33964795+Sakiskid@users.noreply.github.com>
2024-03-04 11:22:54 -0600

    add actiongraph variables

--- boxhead/code/Util/ActionGraphUtilities.cs
+++ boxhead/code/Util/ActionGraphUtilities.cs
@@ -5,12 +5,24 @@ namespace Sandbox.Util
 		/// <summary>
 		/// Returns positiveValue or negativeValue based on incoming bool
 		/// </summary>
-		[ActionGraphNode( "sakiskid.acutils.boolToInt" ), Pure]
+		[ActionGraphNode( "sakiskid.acutils.boolToInt" )]
 		[Title( "Bool To Int" ), Group( "Sakiskid Utilities" ), Icon( "exposure_plus_1" )]
-		public static int GetBoolToInt( bool boolean, int positiveValue = 1, int NegativeValue = 0)
+		public static int BoolToInt( bool boolean, int positiveValue = 1, int NegativeValue = 0)
 		{
 			if ( boolean == true ) return positiveValue;
 			else return NegativeValue;
+		}
+		
+		/// <summary>
+		/// Returns positiveValue or negativeValue based on incoming signal. NOT WORKING
+		/// </summary>
+		[ActionGraphNode( "sakiskid.acutils.boolToInt" ), Pure]
+		[Title( "Signal To Int" ), Group( "Sakiskid Utilities" ), Icon( "exposure_plus_1" )]
+		public static int SignalToInt( object node, int positiveValue = 1, int NegativeValue = 0)
+		{
+			// if ( boolean == true ) return positiveValue;
+			// else return NegativeValue;
+			return 1;
 		}
 	}
 }
