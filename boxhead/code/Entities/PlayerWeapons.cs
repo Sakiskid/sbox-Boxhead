726f8e617890192f55ad605d7e27654264db29c9
Sakiskid <33964795+Sakiskid@users.noreply.github.com>
2024-03-05 15:21:29 -0600

    some weird issue with tasks, but restarting resolved

--- boxhead/code/Entities/PlayerWeapons.cs
+++ boxhead/code/Entities/PlayerWeapons.cs
@@ -3,31 +3,38 @@ using Sandbox;
 
 public sealed class PlayerWeapons : Component
 {
+
+	[Property] public GameObject PistolObject;
 	[Property] public Weapon CurrentWeapon
 	{
 		get;
 		set;
 	}
+	
+	protected override void OnStart()
+	{
+		CurrentWeapon = PistolObject.Components.Get<Weapon>();
+		_ = StartShooting();
+	}
 
 	protected override void OnUpdate()
 	{
-		if ( IsProxy ) return;
 		base.OnUpdate();
+		if ( IsProxy ) return;
 		if ( Input.Down( "attack1" ) )
 		{
 			Shoot();
 		}
 	}
 
-	// Keeping this for async example
-	// private async Task StartShooting()
-	// {
-	// 	while ( true )
-	// 	{
-	// 		await Task.DelaySeconds( 1 );
-	// 		Shoot();
-	// 	}
-	// }
+	private async Task StartShooting()
+	{
+		while ( true )
+		{
+			await Task.DelaySeconds( 1 );
+			Shoot();
+		}
+	}
 
 	private void Shoot()
 	{
